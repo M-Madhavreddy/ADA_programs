f5ddf63465d6f5a4ae694f9342a8c8bd4e91082b
M Madhav Reddy <98480251+M-Madhavreddy@users.noreply.github.com>
2022-08-07 12:04:08 +0530

    Update 01knapsack.cpp

--- programs/01knapsack.cpp
+++ programs/01knapsack.cpp
@@ -47,6 +47,21 @@ void knapsack(int v[], int w[], int n, int W)
         }
         cout<<"\n";
     }
+    i--;
+    j--;
+    while(i>0 && j>=0)
+    {
+        if(k[i][j]==k[i-1][j])
+        {
+            cout<<"x"<<i<<" is not included \n";
+            i--;
+        }
+        else{
+            cout<<"x"<<i<<" is included \n";
+            j=j-w[i];
+            i--;
+        }
+    }
     
 }
 
@@ -61,7 +76,88 @@ int main()
     cin >> W;
 
     int *v = (int *)malloc(n * sizeof(int));
-    int *w = (int *)malloc(n * sizeof(int));
+    int *w = (int *)malloc((n+1) * sizeof(int));
+
+    knapsack(v, w, n, W);
+}#include <iostream>
+using namespace std;
+
+void knapsack(int v[], int w[], int n, int W)
+{
+    int k[100][100];
+
+    cout << "enter your values and weights\n";
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> v[i] >> w[i];
+    }
+
+    int i, j;
+    for (i = 0; i <= n; i++)
+    {
+        for (j = 0; j <= W; j++)
+        {
+
+            if (i == 0 || j == 0)
+            {
+                k[i][j] = 0;
+            }
+            else if (j < w[i])
+            {
+                k[i][j] = k[i - 1][j];
+            }
+            else
+            {
+                if (k[i - 1][j] > k[i - 1][j - w[i]] + v[i])
+                {
+                    k[i][j] = k[i - 1][j];
+                }
+                else
+                {
+                    k[i][j] = k[i - 1][j - w[i]] + v[i];
+                }
+            }
+        }
+    }
+    
+    for (i = 0; i <= n; i++)
+    {
+        for (j = 0; j <=W; j++)
+        {
+            cout<<k[i][j]<<" ";
+        }
+        cout<<"\n";
+    }
+    i--;
+    j--;
+    while(i>0 && j>=0)
+    {
+        if(k[i][j]==k[i-1][j])
+        {
+            cout<<"x"<<i<<" is not included \n";
+            i--;
+        }
+        else{
+            cout<<"x"<<i<<" is included \n";
+            j=j-w[i];
+            i--;
+        }
+    }
+    
+}
+
+int main()
+{
+    int n;
+    cout << "enter the number of objects";
+    cin >> n;
+
+    int W;
+    cout << "enter the maximum weight \n";
+    cin >> W;
+
+    int *v = (int *)malloc(n * sizeof(int));
+    int *w = (int *)malloc((n+1) * sizeof(int));
 
     knapsack(v, w, n, W);
 }
